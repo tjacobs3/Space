a7ac23dcc32c9173781e714c63cb0d490a0e992f
tjacobs3 <thmsjcbs@gmail.com>
2015-04-07 17:53:40 -0500

    update unity for git

---
+++ Assets/Scripts/Debug/ShowBounds.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShowBounds : MonoBehaviour {
+
+    private Vector3 v3FrontTopLeft;
+    private Vector3 v3FrontTopRight;
+    private Vector3 v3FrontBottomLeft;
+    private Vector3 v3FrontBottomRight;
+    private Vector3 v3BackTopLeft;
+    private Vector3 v3BackTopRight;
+    private Vector3 v3BackBottomLeft;
+    private Vector3 v3BackBottomRight;
+
+    private Bounds bounds;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        DrawBox();
+	}
+
+    void CalcPositons()
+    {
+        if (bounds == null)
+            return;
+
+        Vector3 v3Center = bounds.center;
+        Vector3 v3Extents = bounds.extents;
+
+        v3FrontTopLeft = new Vector3(v3Center.x - v3Extents.x, v3Center.y + v3Extents.y, v3Center.z - v3Extents.z);  // Front top left corner
+        v3FrontTopRight = new Vector3(v3Center.x + v3Extents.x, v3Center.y + v3Extents.y, v3Center.z - v3Extents.z);  // Front top right corner
+        v3FrontBottomLeft = new Vector3(v3Center.x - v3Extents.x, v3Center.y - v3Extents.y, v3Center.z - v3Extents.z);  // Front bottom left corner
+        v3FrontBottomRight = new Vector3(v3Center.x + v3Extents.x, v3Center.y - v3Extents.y, v3Center.z - v3Extents.z);  // Front bottom right corner
+        v3BackTopLeft = new Vector3(v3Center.x - v3Extents.x, v3Center.y + v3Extents.y, v3Center.z + v3Extents.z);  // Back top left corner
+        v3BackTopRight = new Vector3(v3Center.x + v3Extents.x, v3Center.y + v3Extents.y, v3Center.z + v3Extents.z);  // Back top right corner
+        v3BackBottomLeft = new Vector3(v3Center.x - v3Extents.x, v3Center.y - v3Extents.y, v3Center.z + v3Extents.z);  // Back bottom left corner
+        v3BackBottomRight = new Vector3(v3Center.x + v3Extents.x, v3Center.y - v3Extents.y, v3Center.z + v3Extents.z);  // Back bottom right corner
+
+        v3FrontTopLeft = transform.TransformPoint(v3FrontTopLeft);
+        v3FrontTopRight = transform.TransformPoint(v3FrontTopRight);
+        v3FrontBottomLeft = transform.TransformPoint(v3FrontBottomLeft);
+        v3FrontBottomRight = transform.TransformPoint(v3FrontBottomRight);
+        v3BackTopLeft = transform.TransformPoint(v3BackTopLeft);
+        v3BackTopRight = transform.TransformPoint(v3BackTopRight);
+        v3BackBottomLeft = transform.TransformPoint(v3BackBottomLeft);
+        v3BackBottomRight = transform.TransformPoint(v3BackBottomRight);
+    }
+
+    void DrawBox()
+    {
+        Debug.DrawLine(v3FrontTopLeft, v3FrontTopRight, Color.green);
+        Debug.DrawLine(v3FrontTopRight, v3FrontBottomRight, Color.green);
+        Debug.DrawLine(v3FrontBottomRight, v3FrontBottomLeft, Color.green);
+        Debug.DrawLine(v3FrontBottomLeft, v3FrontTopLeft, Color.green);
+
+        Debug.DrawLine(v3BackTopLeft, v3BackTopRight, Color.green);
+        Debug.DrawLine(v3BackTopRight, v3BackBottomRight, Color.green);
+        Debug.DrawLine(v3BackBottomRight, v3BackBottomLeft, Color.green);
+        Debug.DrawLine(v3BackBottomLeft, v3BackTopLeft, Color.green);
+
+        Debug.DrawLine(v3FrontTopLeft, v3BackTopLeft, Color.green);
+        Debug.DrawLine(v3FrontTopRight, v3BackTopRight, Color.green);
+        Debug.DrawLine(v3FrontBottomRight, v3BackBottomRight, Color.green);
+        Debug.DrawLine(v3FrontBottomLeft, v3BackBottomLeft, Color.green);
+    }
+
+    public void setBounds(Bounds bounds)
+    {
+        this.bounds = bounds;
+        CalcPositons();
+    }
+}
---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,128 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class LevelGenerator : MonoBehaviour {
+
+    public int numRooms = 30;
+    public int seed = 1337;
+    public Vector2 minSize = new Vector2(2f, 2f);
+    public Vector2 maxSize = new Vector2(10f, 10f);
+    public float maxRatio = 3f;
+    public float totalSizeRadius = 15f;
+    public float stepTime = 0.2f;
+    public Bounds shipBounds = new Bounds(Vector3.zero, new Vector3(20f, 5f, 30f));
+
+    private List<GameObject> rooms = new List<GameObject>();
+    private bool allRoomsSeperated = false;
+    private float lastStep = 0f;
+
+	// Use this for initialization
+	void Start () {
+        // Debug draw the shipBounds
+        ShowBounds sb = GetComponent<ShowBounds>();
+        if (sb != null)
+            sb.setBounds(shipBounds);
+
+        Random.seed = seed;
+        createRooms();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        lastStep += Time.deltaTime;
+        if (lastStep > stepTime)
+        {
+            lastStep = 0f;
+            seperateRooms();
+            if (allRoomsSeperated)
+                removeRoomsNotInBounds();
+        }
+	}
+
+    void createRooms()
+    {
+        while (rooms.Count < numRooms)
+        {
+            GameObject newRoom = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            Vector3 newScale = new Vector3(Mathf.Round(Random.Range(minSize.x, maxSize.x)), 1f, Mathf.Round(Random.Range(minSize.y, maxSize.y)));
+            Vector2 newPosition = Random.insideUnitCircle * totalSizeRadius;
+            newPosition.x = Mathf.Round(newPosition.x);
+            newPosition.y = Mathf.Round(newPosition.y);
+            newRoom.transform.localScale = newScale;
+            newRoom.transform.position = new Vector3(newPosition.x, 0f, newPosition.y);
+            Renderer rend = newRoom.GetComponent<Renderer>();
+            rend.material.SetColor("_Color", new Color(Random.value, Random.value, Random.value));
+
+            if (Mathf.Max(newScale.x, newScale.z) / Mathf.Min(newScale.x, newScale.z) < maxRatio)
+                rooms.Add(newRoom);
+            else
+                Destroy(newRoom);
+        }
+    }
+
+    void seperateRooms()
+    {
+        if (allRoomsSeperated)
+            return;
+
+        allRoomsSeperated = true;
+
+        foreach (GameObject room in rooms)
+        {
+            int collissionCount = 0;
+            Collider[] hitColliders = Physics.OverlapSphere(room.transform.position, Mathf.Max(room.transform.localScale.x, room.transform.localScale.z) * 1.5f);
+
+            if (hitColliders.Length == 1)
+                continue;
+
+            Renderer rend = room.GetComponent<Renderer>();
+            Vector3 com = Vector3.zero;
+            foreach (Collider c in hitColliders)
+            {
+                Renderer rend2 = c.GetComponent<Renderer>();
+                if (c.gameObject != room && rend.bounds.Intersects(rend2.bounds))
+                {
+                    com += c.transform.position;
+                    Debug.DrawLine(room.transform.position, c.transform.position, room.GetComponent<Renderer>().material.color, stepTime, false);
+                    collissionCount++;
+                    allRoomsSeperated = false;
+                }
+            }
+
+            if (collissionCount > 0)
+            {
+                com /= collissionCount;
+                Vector3 move = (room.transform.position - com).normalized;
+                move.x = Mathf.Round(move.x);
+                move.y = Mathf.Round(move.y);
+                move.z = Mathf.Round(move.z);
+                Debug.DrawLine(room.transform.position, room.transform.position + move, Color.white, stepTime, false);
+                room.transform.position = room.transform.position + move;
+            }
+                
+        }
+
+    }
+
+    void removeRoomsNotInBounds()
+    {
+        rooms.RemoveAll(room => {
+            Renderer rend = room.GetComponent<Renderer>();
+            bool remove = !shipBounds.Contains(rend.bounds.min) || !shipBounds.Contains(rend.bounds.max);
+            if (remove)
+                Destroy(room.gameObject);
+            return remove;
+        });
+    }
+
+    Vector3 centerOfMass(Collider[] colliders)
+    {
+        Vector3 p = Vector3.zero;
+        foreach(Collider c in colliders) {
+            p += c.transform.position;
+        }
+        p /= colliders.Length;
+        return p;
+    }
+}
