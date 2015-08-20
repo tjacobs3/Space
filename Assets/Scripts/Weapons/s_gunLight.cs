b52732ca6101a4247ae972605edb8947e82ba8b8
tjacobs3 <thmsjcbs@gmail.com>
2015-08-19 23:25:39 -0500

    Switch to ufps

---
+++ Assets/Scripts/Weapons/s_gunLight.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class s_gunLight : MonoBehaviour {
+	public Light flashLight;
+	
+	public void turnOff() {
+		flashLight.enabled = false;
+	}
+
+	public void setLight(bool onOff) {
+		flashLight.enabled = onOff;
+	}
+}
---
+++ Assets/Scripts/Weapons/s_gunLightController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class s_gunLightController : MonoBehaviour {
+	public bool lightEnabled = true;
+
+	vp_FPPlayerEventHandler m_Player;
+	void Awake()
+	{
+		m_Player = transform.GetComponent<vp_FPPlayerEventHandler>();
+	}
+	
+	protected virtual void OnEnable()
+	{
+		if (m_Player != null)
+			m_Player.Register(this);
+	}
+	protected virtual void OnDisable()
+	{
+		if (m_Player != null)
+			m_Player.Unregister(this);
+	}
+	
+	void OnMessage_ToggleLight()
+	{
+		lightEnabled = !lightEnabled;
+		s_gunLight[] lights = transform.GetComponentsInChildren<s_gunLight> ();
+		foreach (s_gunLight light in lights) {
+			light.setLight(lightEnabled);
+		}
+	}
+}
